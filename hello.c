cd8525bc556af6d4df9e54faad99468e8522e115
abhiy7002 <33065702+abhiy7002@users.noreply.github.com>
2017-10-24 22:30:56 +0530

    Update hello.c
    Some changes made in this code.

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-print("Hello world...!!!");
+ print("Hello world...!!!");
   
-printf("\nThis is the basic C program");
+ printf("\nThis is the basic C program");
 }
